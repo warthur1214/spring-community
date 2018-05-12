40be21e0e05a97cc390fa7066abce33cc7c8261a
warthur <warthur@163.com>
2018-05-12 22:42:35 +0800

    update zuul configuration

--- spring-wechat/src/main/java/com/warthur/community/wechat/WechatApplication.java
+++ spring-wechat/src/main/java/com/warthur/community/wechat/WechatApplication.java
@@ -3,9 +3,12 @@ package com.warthur.community.wechat;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by warthur on 2018/5/8.
@@ -16,6 +19,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan({"com.warthur.community.common.framework", "com.warthur.community.wechat"})
 @MapperScan("com.warthur.community.wechat.dao")
 public class WechatApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WechatApplication.class, args);
