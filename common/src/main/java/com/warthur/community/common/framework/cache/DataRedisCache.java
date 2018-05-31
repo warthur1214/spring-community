0e896292a73b93b61f6fb46146f800dea22d97fb
warthur1214 <warthur@163.com>
2018-05-31 21:46:44 +0800

    细节调整

--- common/src/main/java/com/warthur/community/common/framework/cache/DataRedisCache.java
+++ common/src/main/java/com/warthur/community/common/framework/cache/DataRedisCache.java
@@ -1,5 +1,7 @@
 package com.warthur.community.common.framework.cache;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.*;
 import org.springframework.stereotype.Component;
 
